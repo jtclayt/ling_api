cb4e00cbfd0e55d112d52f76d34fb3a2fb9075d7
jtclayt <jt.clayton92@yahoo.com>
2021-12-21 15:09:21 -0800

    Update app insights config

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
-import appInsights from "applicationinsights";
+import * as appInsights from "applicationinsights";
 import dotenv from "dotenv";
 import express, { Application, Request, Response } from "express";
 import path from "path";
 
 import { KeyVaultClient } from "./clients/keyvault-client";
 
-appInsights.setup().start();
+appInsights.setup()
+  .setAutoDependencyCorrelation(true)
+  .setAutoCollectRequests(true)
+  .setAutoCollectPerformance(true, true)
+  .setAutoCollectExceptions(true)
+  .setAutoCollectDependencies(true)
+  .setAutoCollectConsole(true)
+  .setUseDiskRetryCaching(true)
+  .setSendLiveMetrics(false)
+  .setDistributedTracingMode(appInsights.DistributedTracingModes.AI)
+  .start();
 dotenv.config({ path: path.resolve(process.cwd(), 'env/.env')});
 const app: Application = express();
 const port: number = Number(process.env.PORT) || 5000;
