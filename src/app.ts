41b418816e01a2fe9bdb7597eee15a6ccfd64072
jtclayt <jt.clayton92@yahoo.com>
2021-12-21 09:49:42 -0800

    Basic keyvault client implemented

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv";
 import path from "path";
+import { KeyVaultClient } from "./clients/keyvault-client";
 
 dotenv.config({ path: path.resolve(process.cwd(), 'env/.env')});
 const app: Application = express();
 const port: number = Number(process.env.PORT) || 5000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ data: "hello" });
+app.get("/", async (req: Request, res: Response) => {
+  const secret = await KeyVaultClient.instance.getSecretAsync("Test");
+  res.status(200).json({ data: secret });
 });
 
 app.listen(port, () => {
---
+++ src/clients/keyvault-client.ts
@@ -0,0 +1,21 @@
+import { DefaultAzureCredential } from "@azure/identity";
+import { KeyVaultSecret, SecretClient } from "@azure/keyvault-secrets";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(process.cwd(), 'env/.env')});
+
+export class KeyVaultClient {
+  public static instance: KeyVaultClient = new KeyVaultClient();
+  private secretClient: SecretClient;
+
+  constructor() {
+    const credential = new DefaultAzureCredential();
+    const url = `https://${process.env.KEY_VAULT_NAME}.vault.azure.net`;
+    this.secretClient = new SecretClient(url, credential);
+  }
+
+  async getSecretAsync(secretName: string): Promise<KeyVaultSecret> {
+    return await this.secretClient.getSecret(secretName);
+  }
+}
